171d4c86fce0141d6b0712ba3bc20afbc3f9f0e2
Mohammad <72628758+mohammadhj98@users.noreply.github.com>
2021-03-22 17:48:50 +0430

    Update Conver Decimal number to Binary number.py

--- Conver Decimal number to Binary number.py
+++ Conver Decimal number to Binary number.py
@@ -11,16 +11,3 @@ while number != 0:
     number = number // 2
     result = str(remainder) + result
 print("The binary representation is", result)
-#-----------------------------------------
-# N=int(input("Enter a positive number in base 10: "))
-# while(N<0):
-#     N=int(input("Enter a positive number in base 10: "))
-# #------------------------------------
-# print("ravesh 1: Binary is: "+str(bin(N))[2:])
-# #-----------------or-----------------
-# r=''
-# while(N>1):
-#     r=r+str(N%2)
-#     N=N//2
-# r=r+str(N)
-# print("ravesh 2: Binary is: "+r[::-1])
